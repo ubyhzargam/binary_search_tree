0b1ce0b84e80e8117be2fe9bcf42a068c0432727
ubyhzargam <ubyhzargam@gmail.com>
2023-11-20 19:22:36 +0530

    Update main.cpp

--- binary_search_tree/main.cpp
+++ binary_search_tree/main.cpp
@@ -8,6 +8,34 @@ struct Node
     Node *rchild;
 }*root=NULL;
 
+struct Node *Search(int key)
+{
+    struct Node *t;
+    t=root;
+    while(t!=NULL)
+    {
+        if(key==t->data)
+            return t;
+        else if(key<t->data)
+            t=t->lchild;
+        else
+            t=t->rchild;
+    }
+    return NULL;
+}
+
+struct Node *RecursiveSearch(Node *t,int key)
+{
+    if(t==NULL)
+        return NULL;
+    else if(key==t->data)
+        return t;
+    else if(key<t->data)
+        return RecursiveSearch(t->lchild,key);
+    else
+        return RecursiveSearch(t->rchild,key);
+}
+
 void Insert(int key)
 {
     struct Node *p;
@@ -70,11 +98,24 @@ void Inorder(struct Node *p)
 
 int main()
 {
+    int key;
     root=RecursiveInsert(root,10);
     RecursiveInsert(root,5);
     RecursiveInsert(root,20);
     RecursiveInsert(root,8);
     RecursiveInsert(root,30);
-    Inorder(root);
+    Inorder(root);cout<<endl;
+    cout<<"Enter the element you want to search in the tree using Iterative method : "<<endl;
+    cin>>key;
+    if(Search(key))
+        cout<<"The element "<<key<<" is present in the Tree "<<endl;
+    else
+        cout<<"The element "<<key<<" is not present in the Tree"<<endl;
+    cout<<"Enter the element you want to search in the tree using Recursive method : "<<endl;
+    cin>>key;
+    if(RecursiveSearch(root,key))
+        cout<<"The element "<<key<<" is preent in the Tree"<<endl;
+    else
+        cout<<"The element "<<key<<" is not present in the Tree"<<endl;
     return 0;
 }
